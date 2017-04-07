ef9016270fc24bda95298fbf89071c3752ac01b0
mwpark2014 <mwp2010@gmail.com>
2017-04-07 15:26:25 -0400

    Some changes to the activities

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //TODO: Send Message
+    public void sendMessage(View view) {
+        // Do something
     }
 }

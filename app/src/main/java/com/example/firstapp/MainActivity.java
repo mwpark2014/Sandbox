0ca12be4fc6bf03e33c818bfbe5f620750c63466
mwpark2014 <mwp2010@gmail.com>
2017-04-10 17:23:06 -0400

    Udacity Graphics Course edits

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.firstapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
